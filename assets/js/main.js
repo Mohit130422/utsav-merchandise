4bb21483ddbf8bee1dec5b0ea2129a3623d21a2f
Ankit Varshney <ankitvarshney1810@gmail.com>
2022-09-21 01:19:18 +0530

    course-list-page-done

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,58 +15,68 @@ $(document).ready(function () {
     });
     $("#close, main").click(function () {
         $("#box").hide();
+        $(".sidebar").removeClass('openside');
+        $("#act-box").hide();
     });
     $(document).ready(function(){
         $(".btnmenu").click(function(){
             $(".sidebar").toggleClass('openside');
         });
     });
-    $("#opt").click(function () {
-        $("#opt-box").show();
-    });
-    $("#follow").click(function () {
-        $("#opt-box").hide();
-    });
-    $("#ask").click(function () {
-        $("#ask-question").show();
-        $("body").addClass("overflow-hidden");
+
+    $('#recommended-slide').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        lazyLoad:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
     })
-    $("#inspire").click(function () {
-        $("#post-inspiration").show();
-        $("body").addClass("overflow-hidden");
+    $('#trending-slide').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        lazyLoad:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
     })
-    $("#add-img").click(function () {
-        $("#add-image").show();
-    })
-    $(".close-modal").click(function () {
-        $(".modal").hide();
-        $("body").removeClass("overflow-hidden");
-    })
-    $("#cat-option").click(function () {
-        $("#category").toggle();
-    })
-    $("#add-image").click(function () {
-        $("#category").hide();
+    $('#teachers-slide').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        lazyLoad:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
     })
 
-    $(".like-stat").click(function () {
-        $("#post-likes").show();
-        $("body").addClass("overflow-hidden");
-    })
-    $(".comment").click(function () {
-        $(".user-comments").toggle();
-    })
-
-    $(".category").click(function () {
-        var id = $(this).attr('id');
-        var mySecondDiv = $("<li><input type='checkbox' id='" + id + "'><label class='selected-cat' id='" + id + "' for='" + id + "'>" + id + "<i class='fas fa-times' id='close'></i></label></li>");
-        $("#selected-category").append(mySecondDiv);
-        $("." + id).hide();
-    })
-
-    $('.selected-cat').on('click', function () {
-        var target = $(this).attr('for');
-        console.log(target)
-        $("#" + target).hide().siblings(".category").show();
-    });
 });
